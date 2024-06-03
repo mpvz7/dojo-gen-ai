6b9ac42d7db4427f21807d356495e97f445944fb
Mariam <67973183+mpvz7@users.noreply.github.com>
2024-06-03 15:02:34 +0000

    dojo

--- backend/modules/recorder/recorder.py
+++ backend/modules/recorder/recorder.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
+from backend.modules.recorder.utils.embed_and_save_chunks import embed_and_save_chunks
+from backend.modules.recorder.utils.generate_chunks import generate_chunks
+from backend.modules.recorder.utils.get_answer import get_answer
 from backend.modules.recorder.utils.get_recorded_audio import get_recorded_audio
+from backend.modules.recorder.utils.get_transcription import get_transcription
+from backend.modules.recorder.utils.save_question_and_response import ( save_question_and_response )
 
 
 def recorder():
@@ -20,4 +25,15 @@ def recorder():
 
     if audio_data is not None:
         # write audio data
-        print("audio_data is", audio_data)
+        transcription = get_transcription(audio_data)
+        st.write("Transcription:", transcription)
+
+        transcription_chunks = generate_chunks(transcription)
+
+        embed_and_save_chunks(transcription_chunks)
+
+    question = st.text_input(label="Question", placeholder="Enter a question please")
+    if question:
+        answer = get_answer(question)
+        save_question_and_response(question, answer)
+        st.write("Answer:", answer)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,40 @@
-import React from "react"
+//App.tsx
+
+import AudioReactRecorder, { RecordState } from 'audio-react-recorder';
+import 'audio-react-recorder/dist/index.css';
+import React, { useState } from "react";
+import { Streamlit } from "streamlit-component-lib";
 
 export const App = () => {
-  return <div>Ready to build your AI note taker ? Let's go 🚀🚀🔥</div>
-}
+  const [recordState, setRecordState] = useState(null);
+  const [audioDataURL, setAudioDataURL] = useState('');
+
+  const onClick = () => {
+    if (recordState === RecordState.START) {
+      setRecordState(RecordState.STOP);
+    } else {
+      setAudioDataURL('');
+      setRecordState(RecordState.START);
+    }
+  };
+
+  const onStop = (data) => {
+    setAudioDataURL(data.url);
+    data.blob.arrayBuffer().then((arrayBuffer) => {
+       const uint8Array = new Uint8Array(arrayBuffer);
+        Streamlit.setComponentValue({ "arr": uint8Array });
+     });
+  };
+  
+  const recordButtonLabel = recordState === RecordState.START ? 'Stop' : 'Start'
+
+  return (
+    <div>
+      <AudioReactRecorder state={recordState} onStop={onStop}/>
+      <div>
+        <audio id='audio' controls src={audioDataURL} />
+        <button id='toggle' onClick={onClick}>{recordButtonLabel}</button>
+      </div>
+    </div>
+  );
+};
